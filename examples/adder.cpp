03d368619140ccc81d5f1a23427e606e41f6e4c5
Naoto Aoki <184037301+naoto-aoki-fy@users.noreply.github.com>
2025-08-03 23:12:23 +0900

    Add C++ examples for adder, qft, and vqe

---
+++ examples/adder.cpp
@@ -0,0 +1,55 @@
+#include "qasm.hpp"
+
+class userqasm : public qasm::qasm
+{
+public:
+    void circuit() {
+        using namespace qasm;
+        
+        qubits cin = qalloc(1);
+        qubits a = qalloc(4);
+        qubits b = qalloc(4);
+        qubits cout = qalloc(1);
+        void majority(qubit<> a, qubit<> b, qubit<> c) {
+            cx()(c, b);
+            cx()(c, a);
+            ccx()(a, b, c);
+        }
+        
+        void unmaj(qubit<> a, qubit<> b, qubit<> c) {
+            ccx()(a, b, c);
+            cx()(c, a);
+            cx()(a, b);
+        }
+        
+        bit<5> ans;
+        uint<4> a_in = 1;
+        uint<4> b_in = 15;
+        reset(cin);
+        reset(a);
+        reset(b);
+        reset(cout);
+        for (unsigned int i : slice(0, 3)) {
+            if (((bool)(a_in[i]))) {
+                x()(a[i]);
+            }
+            if (((bool)(b_in[i]))) {
+                x()(b[i]);
+            }
+        }
+        majority()(cin[0], b[0], a[0]);
+        for (unsigned int i : slice(0, 2)) {
+            majority()(a[i], b[i + 1], a[i + 1]);
+        }
+        cx()(a[3], cout[0]);
+        for (unsigned int i : slice(2, -1, 0)) {
+            unmaj()(a[i], b[i + 1], a[i + 1]);
+        }
+        unmaj()(cin[0], b[0], a[0]);
+        ans[slice(0, 3)] = measure(b[slice(0, 3)]);
+        ans[4] = measure(cout[0]);
+        
+    }
+};
+
+extern "C" qasm::qasm* constructor() { return new userqasm(); }
